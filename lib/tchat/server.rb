0faa9040b662e2134a94df50366b2d684a0de1a4
Tiago Alves <tiagoadalves@gmail.com>
2010-10-07 00:14:29 +0100

    worked a little on the server class and added a client_connection handler

--- lib/tchat/server.rb
+++ lib/tchat/server.rb
@@ -1,9 +1,41 @@
+require 'socket'
+require './client_connection.rb'
+
 module TChat
 
   class Server
 
-    def initialize
+    @users
 
+    def initialize(port)
+      @port = port
+    end
+
+    def start
+      server = TCPServer.open(@port)
+
+      loop {
+
+        Thread.start(server.accept) do |client|
+          client_line = client.gets
+          client_args = client_line.split(' ')
+
+          if (client_args.length < 3) {
+            client.puts "Invalid sigin information provided..."
+            client.close
+            break
+          }
+
+          arg_command = client_args[0]
+          arg_email = client_args[1]
+          arg_password = client_args[2]
+
+          client.puts "echo... " + client_line 
+	        client.puts "Closing the connection. Bye!"
+          client.close
+        end
+
+      }
     end
 
   end #class Server
